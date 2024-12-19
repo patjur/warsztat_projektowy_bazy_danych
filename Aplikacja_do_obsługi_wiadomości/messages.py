0cad51589f009b7d3cc6ec94b9ef35abfa4f22cc
patryk <patjur4032@gmail.com>
2024-12-19 19:13:42 +0100

    Added functions: print_user_messages, send_message

--- Aplikacja_do_obsługi_wiadomości/messages.py
+++ Aplikacja_do_obsługi_wiadomości/messages.py
@@ -1,5 +1,7 @@
 import argparse
 
+from models import User, Message
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--username", help="username")
 parser.add_argument("-p", "--password", help="password (min 8 characters)")
@@ -8,3 +10,27 @@ parser.add_argument("-t", "--to", help="to")
 parser.add_argument("-s", "--send", help="text message to send")
 
 args = parser.parse_args()
+
+
+def print_user_messages(cur, user):
+    messages = Message.load_all_messages(cur, user.id)
+    for message in messages:
+        from_ = User.load_user_by_id(cur, message.from_id)
+        print(20 * "-")
+        print(f"from: {from_.username}")
+        print(f"data: {message.creation_date}")
+        print(message.text)
+        print(20 * "-")
+
+
+def send_message(cur, from_id, recipient_name, text):
+    if len(text) > 255:
+        print("Message is too long!")
+        return
+    to = User.load_user_by_username(cur, recipient_name)
+    if to:
+        message = Message(from_id, to.id, text=text)
+        message.save_to_db(cur)
+        print("Message send")
+    else:
+        print("Recipient does not exists.")
